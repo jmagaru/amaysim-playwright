cf45437cacf4bc6052914daa95282fd935922550
Jonathan Magaru <phmagjon@carlsberggroup.com>
2023-03-27 20:07:09 +0800

    Fix: keyboard press on ctrl, with variation of os

--- tests/amaysim.spec.js
+++ tests/amaysim.spec.js
@@ -1,6 +1,8 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
+const os = require('os');
 const env = require('../data.json')
+let controlKey = 'Control'
 
 test.describe("Refer A Friend", ()=>{
     test.beforeEach(async ({page}) =>{
@@ -9,18 +11,22 @@ test.describe("Refer A Friend", ()=>{
         await page.getByLabel('amaysim number or email').fill(env.amaysim_number);
         await page.getByLabel('Password').fill(env.password);
         await page.getByRole('button', { name: 'Login' }).click();
-        await page.getByText('0466 134 57415GB Data Plan').click();
+        await page.getByText('UAT2 - 0466134574').click();
         await page.getByRole('link', { name: 'Refer a friend' }).click();
     });
 
-    test("Referral Link Can Be Copied By Clicking Copy Button", async ({page})=>{
-        const referFrame = await page.frameLocator('[title="Powered By Friendbuy"]');
+     test("Referral Link Can Be Copied By Clicking Copy Button", async ({page})=>{
+        const referFrame = page.frameLocator('[title="Powered By Friendbuy"]');
         const referralCode = await referFrame.locator('#r > span > p > a').getAttribute('href');
+        const platform = os.platform();
         await referFrame.locator('#s > span > p').click()
         await new Promise(resolve => setTimeout(resolve, 5000));
         await referFrame.locator('#a').focus();
-        await page.keyboard.press('Control+KeyV')
-        await expect(await referFrame.locator('#a').inputValue()).toEqual(referralCode);
+        if(platform=='darwin'){
+             controlKey='Meta';
+        }
+        await page.keyboard.press(`${controlKey}+KeyV`)
+        expect(await referFrame.locator('#a').inputValue()).toEqual(referralCode);
     });
 
     test("Confirmation Message Should Displayed On Successful Refer A Friend", async ({page})=>{
